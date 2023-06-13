7e6547e8d511f0d9110f53b4547f977e038017f9
Jorge Silva <14844897+JorgeSilva7@users.noreply.github.com>
2023-06-13 18:01:44 -0400

    update backend url

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,7 +27,7 @@
 Cypress.Commands.add("login", (email, password) => {
 	cy.request({
 		method: "POST",
-		url: `http://localhost:3000/login`,
+		url: `http://3.89.221.7:3000/login`,
 		body: {
 			email,
 			password,
